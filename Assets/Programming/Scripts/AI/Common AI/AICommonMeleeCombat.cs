4d5d2920d0e3cd04534790f968a2770613b4f366
DM <dommcneill@outlook.com>
2025-01-31 23:07:24 +0000

    basic melee movement. fixes to AI Base.

--- Assets/Programming/Scripts/AI/Common AI/AICommonMeleeCombat.cs
+++ Assets/Programming/Scripts/AI/Common AI/AICommonMeleeCombat.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AICommonMeleeCombat : AIBase
 {
+	[Header("Attacking")]
+	[SerializeField]
+	protected float Damage = 10f;
+	/// <summary>
+	/// How long before next attack.
+	/// </summary>
+	[Tooltip("How long before next attack.")]
+	[SerializeField]
+	protected float AttackRate = 10;
 
+	protected NavMeshPath path;
 
+	protected override void Update()
+	{
+		base.Update();
+
+		if (NavMesh.CalculatePath(transform.position, GameObject.FindWithTag("Player").transform.position, NavMesh.AllAreas, path))
+			Agent.path = path;
+	}
 }
