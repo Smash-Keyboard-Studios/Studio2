96f6435d7520f05333a9b074cee5ed9dc7e45c7e
Snowballgamzzz <2winga47@solent.ac.uk>
2025-01-31 22:37:45 +0000

    Basic Player
    created a basic player with basic movement

--- Assets/Programming/Character/PlayerMovement.cs
+++ Assets/Programming/Character/PlayerMovement.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintMultiplier = 2.0f;
+
+    [Header("Jump Parameters")]
+    [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private float gravity = 9.81f;
+
+    private CharacterController characterController;
+    private PlayerInputHandler inputHandler;
+    private Vector3 currentMovement;
+
+    private void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        inputHandler = PlayerInputHandler.Instance;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        HandleMovement();
+    }
+    
+    void HandleMovement()
+    {
+        float speed = walkSpeed * (inputHandler.SprintValue > 0 ? sprintMultiplier : 1f);
+
+        Vector3 inputDirection = new Vector3(inputHandler.MoveInput.x, 0f, inputHandler.MoveInput.y);
+        Vector3 worldDirection = transform.TransformDirection(inputDirection);
+        worldDirection.Normalize();
+
+        currentMovement.x = worldDirection.x * speed;
+        currentMovement.z = worldDirection.z * speed;
+
+        HandleJumping();
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    void HandleJumping()
+    {
+        if (characterController.isGrounded)
+        {
+            currentMovement.y = -0.5f;
+
+            if (inputHandler.JumpTriggered)
+            {
+                currentMovement.y = jumpForce;
+            }
+        }
+        else
+        {
+            currentMovement.y -= gravity * Time.deltaTime;
+        }
     }
 }
