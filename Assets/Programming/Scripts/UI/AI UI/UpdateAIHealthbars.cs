f8c8ffe07c0ff16a0793887e08cd3d5f8a8f5dc6
DM <dommcneill@outlook.com>
2025-02-09 05:27:01 +0000

    AI Healthbars work
    temp UI in place

---
+++ Assets/Programming/Scripts/UI/AI UI/UpdateAIHealthbars.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//by    _                 _ _                     
+//     | |               (_) |                    
+//   __| | ___  _ __ ___  _| |__  _ __ ___  _ __  
+//  / _` |/ _ \| '_ ` _ \| | '_ \| '__/ _ \| '_ \ 
+// | (_| | (_) | | | | | | | |_) | | | (_) | | | |
+//  \__,_|\___/|_| |_| |_|_|_.__/|_|  \___/|_| |_|
+
+
+
+/// <summary>
+/// Sets the healthbar on the AI to match the current health.
+/// </summary>
+public class UpdateAIHealthbars : MonoBehaviour
+{
+	private AIBase aiBase;
+
+	public Slider healthBar;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		aiBase = GetComponent<AIBase>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		healthBar.value = (aiBase.currentHealth / aiBase.maxHealth);
+	}
+}
