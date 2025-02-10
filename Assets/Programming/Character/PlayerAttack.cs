8f0d00d79a07605cefb2e71ee55010b4232b93dd
Odin <odin.dioszegi@gmail.com>
2025-02-10 16:09:49 +0000

    Simple Combat system + camera arm

---
+++ Assets/Programming/Character/PlayerAttack.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    private AttackSpace space;
+
+    private void Awake()
+    {
+        space = GetComponentInChildren<AttackSpace>();
+    }
+    //private Animator animator;
+
+    //private void Awake()
+    //{
+    //    animator = GetComponent<Animator>();
+    //}
+
+    public void OnAttack(InputValue input)
+    {
+        foreach (var target in space.TakeDamageInRange.ToList<IDamagable>())
+        {
+            target.TakeDamage(2);
+        }
+        Debug.Log("Attack"); //change these to the animations that will be added later
+    }
+
+    public void OnHeavyAttack(InputValue input)
+    {
+        foreach (var target in space.TakeDamageInRange.ToList<IDamagable>())
+        {
+            target.TakeDamage(5);
+        }
+        Debug.Log("Heavy Attack"); //change these to the animations that will be added later
+    }
+}
