a36269d26abf033b90fa327dacb997510c7be018
DM <dommcneill@outlook.com>
2025-02-14 00:37:05 +0000

    added some comments, move design back to normal location

--- Assets/Programming/Enemies/Scripts/Common AI/Melee/AICommonMeleeAudio.cs
+++ Assets/Programming/Enemies/Scripts/Common AI/Melee/AICommonMeleeAudio.cs
@@ -3,6 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//by    _                 _ _                     
+//     | |               (_) |                    
+//   __| | ___  _ __ ___  _| |__  _ __ ___  _ __  
+//  / _` |/ _ \| '_ ` _ \| | '_ \| '__/ _ \| '_ \ 
+// | (_| | (_) | | | | | | | |_) | | | (_) | | | |
+//  \__,_|\___/|_| |_| |_|_|_.__/|_|  \___/|_| |_|
+
+
+
+
+/// <summary>
+/// Audio event system for the common melee AI.
+/// </summary>
 public class AICommonMeleeAudio : AIAudioBase
 {
 	protected AICommonMeleeCombat aiCommonMeleeCombat;
--- Assets/Programming/Enemies/Scripts/Core/AIAudioBase.cs
+++ Assets/Programming/Enemies/Scripts/Core/AIAudioBase.cs
@@ -3,6 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//by    _                 _ _                     
+//     | |               (_) |                    
+//   __| | ___  _ __ ___  _| |__  _ __ ___  _ __  
+//  / _` |/ _ \| '_ ` _ \| | '_ \| '__/ _ \| '_ \ 
+// | (_| | (_) | | | | | | | |_) | | | (_) | | | |
+//  \__,_|\___/|_| |_| |_|_|_.__/|_|  \___/|_| |_|
+
+
+
+/// <summary>
+/// The base class for audio listeners.
+/// </summary>
 public class AIAudioBase : MonoBehaviour
 {
 	protected AIBase aiBase;
