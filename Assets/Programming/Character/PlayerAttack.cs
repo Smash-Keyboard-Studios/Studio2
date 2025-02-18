49170909fc8640e1624a2f2613276402060029f8
odin <odin.dioszegi@gmail.com>
2025-02-18 15:41:12 +0000

    Added weapon delay
    Added light and heavy weapon delay that can be changed in editor
    
    made it so damage numbers can be changed in editor

--- Assets/Programming/Character/PlayerAttack.cs
+++ Assets/Programming/Character/PlayerAttack.cs
@@ -7,11 +7,17 @@ using UnityEngine.InputSystem;
 public class PlayerAttack : MonoBehaviour
 {
 
-    private AttackSpace space;
+    private AttackSpace space; // the area of where you attack (the attack space)
+    [Header("DamageNumbers")]
+    public int LightDmg = 2;
+    public int HeavyDmg = 5;
+    public float LightAtkDelay;
+    public float HeavyAtkDelay;
+    public bool isAttacking = false;
 
     private void Awake()
     {
-        space = GetComponentInChildren<AttackSpace>();
+        space = GetComponentInChildren<AttackSpace>(); // the area of where you attack (the attack space)
     }
     //private Animator animator;
 
@@ -22,19 +28,42 @@ public class PlayerAttack : MonoBehaviour
 
     public void OnAttack(InputValue input)
     {
-        foreach (var target in space.TakeDamageInRange.ToList<IDamageable>())
-        {
-            target.TakeDamage(2);
-        }
-        Debug.Log("Attack"); //change these to the animations that will be added later
+        if (isAttacking) return;
+        StartCoroutine(LightAtk());
     }
 
     public void OnHeavyAttack(InputValue input)
     {
+        if (isAttacking) return;
+        StartCoroutine (HeavyAtk());
+    }
+
+    IEnumerator LightAtk()
+    {
+        isAttacking = true; 
+
         foreach (var target in space.TakeDamageInRange.ToList<IDamageable>())
         {
-            target.TakeDamage(5);
+            target.TakeDamage(LightDmg);
         }
-        Debug.Log("Heavy Attack"); //change these to the animations that will be added later
+        //Debug.Log("Attack"); //change these to the animations that will be added later
+        yield return new WaitForSeconds(LightAtkDelay);
+
+        isAttacking = false;
+    }
+
+    IEnumerator HeavyAtk()
+    {
+        isAttacking = true;
+
+        foreach (var target in space.TakeDamageInRange.ToList<IDamageable>())
+        {
+            target.TakeDamage(HeavyDmg);
+        }
+        //Debug.Log("Heavy Attack"); //change these to the animations that will be added later
+        yield return new WaitForSeconds(HeavyAtkDelay);
+
+        isAttacking = false;
     }
 }
+
