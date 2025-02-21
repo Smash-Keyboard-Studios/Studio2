5e103cc1fcb80b3a614c2226d042d559b8bbda6a
DM <dommcneill@outlook.com>
2025-02-21 04:17:26 +0000

    Added a way to use specific spawn locations when spawning a wave of AI

--- Assets/Programming/Enemies/Scripts/Spawn System/SpawnWaveOnStart.cs
+++ Assets/Programming/Enemies/Scripts/Spawn System/SpawnWaveOnStart.cs
@@ -38,10 +38,13 @@ public class SpawnWaveOnStart : MonoBehaviour
 	[SerializeField, Range(0, 20), Tooltip("How random the sequence will be. Set to 0 to disable.")]
 	private int sequenceShuffleAmount = 2;
 
+	[SerializeField, Tooltip("All the spawn locations the AI are allow to spawn at")]
+	private List<GameObject> spawnPoints;
+
 	IEnumerator Start()
 	{
 		yield return new WaitForSeconds(1);
 		if (AISpawnSystem.singleton != null)
-			AISpawnSystem.singleton.SpawnWave(waveData, timeBetweenSpawn, minSpawnRadius, maxSpawnRadius, sequenceShuffleAmount);
+			AISpawnSystem.singleton.SpawnWave(waveData, spawnPoints, timeBetweenSpawn, minSpawnRadius, maxSpawnRadius, sequenceShuffleAmount);
 	}
 }
