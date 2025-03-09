3cccb7b867e4f4a0ba1df46056845e2c5055a507
Soph <sophearle16@gmail.com>
2025-03-09 22:18:09 +0000

    added game menu

--- Assets/Programming/UI/Menus/ESCGameMenu.cs
+++ Assets/Programming/UI/Menus/ESCGameMenu.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ESCMainMenu : MonoBehaviour
+public class ESCGameMenu : MonoBehaviour
 {
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
         {
-            UIManager.Instance.PressMainMenu();
+            UIManager.Instance.PressGameMenu();
         }
     }
 }
