b6a83a6e85312b9082e7b37f5bab87c464ff94ea
DM <dommcneill@outlook.com>
2025-03-31 20:59:06 +0100

    added some comments

--- Assets/Programming/Level Loading System/HotReloader.cs
+++ Assets/Programming/Level Loading System/HotReloader.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//by    _                 _ _                     
+//     | |               (_) |                    
+//   __| | ___  _ __ ___  _| |__  _ __ ___  _ __  
+//  / _` |/ _ \| '_ ` _ \| | '_ \| '__/ _ \| '_ \ 
+// | (_| | (_) | | | | | | | |_) | | | (_) | | | |
+//  \__,_|\___/|_| |_| |_|_|_.__/|_|  \___/|_| |_|
+
+
 /// <summary>
 /// Allows developers working on levels to click play on their level and load the level loader.
 /// DO NOT use this on scenes that are not on the build index!
