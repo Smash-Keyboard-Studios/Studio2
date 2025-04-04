c6370bddb044194b298a8e99587ba3eb78730ba7
MYAUNTIESPASSIVEAGGRESSIVE <adamkw123lightning@gmail.com>
2025-04-04 20:18:15 +0100

    More SFX Settings work
    - added functionality to script
    - need to test

--- Assets/Programming/UI/Menus/VolumeSettings.cs
+++ Assets/Programming/UI/Menus/VolumeSettings.cs
@@ -14,7 +14,29 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider GameplaySlider;
     [SerializeField] private Slider MusicSlider;
 
+    // sets the master volume according to the slider.
+    public void SetMasterVolume()
+    {
+        float volume = MainSlider.value;
 
+        audioMixer.SetFloat("MasterMix", Mathf.Log10(volume) * 20);
+    }
+
+    // sets the music volume according to the slider.
+    public void SetMusicVolume()
+    {
+        float volume = MainSlider.value;
+
+        audioMixer.SetFloat("MusicMix", Mathf.Log10(volume) * 20);
+    }
+
+    // sets the gameplay volume according to the slider.
+    public void SetGameplayVolume() 
+    {
+        float volume = MainSlider.value;
+
+        audioMixer.SetFloat("GameplayMix", Mathf.Log10(volume) * 20);
+    }
 
 
 }
