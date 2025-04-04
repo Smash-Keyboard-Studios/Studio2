5d70df9915b1acc7db14c09fc6c3169f2ab0a360
MYAUNTIESPASSIVEAGGRESSIVE <adamkw123lightning@gmail.com>
2025-04-04 12:50:07 +0100

    UI Fixes and VolumeSettingScript

---
+++ Assets/Programming/UI/Menus/VolumeSettings.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [Header("Audio Mixer")]
+    [SerializeField] private AudioMixer audioMixer;
+
+    [Header("Sliders")]
+    [SerializeField] private Slider MainSlider;
+    [SerializeField] private Slider GameplaySlider;
+    [SerializeField] private Slider MusicSlider;
+
+
+
+
+}
