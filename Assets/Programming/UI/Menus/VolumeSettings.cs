5226f41fe5f53529bde78ec1c6c0c2794591244b
MYAUNTIESPASSIVEAGGRESSIVE <adamkw123lightning@gmail.com>
2025-05-29 14:47:47 +0100

    Audio Setting Script Re-work
    Fixed up the audio script to have a save function and also just more efficent and better use of the sliders.

--- Assets/Programming/UI/Menus/VolumeSettings.cs
+++ Assets/Programming/UI/Menus/VolumeSettings.cs
@@ -10,33 +10,68 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private AudioMixer audioMixer;
 
     [Header("Sliders")]
-    [SerializeField] private Slider MainSlider;
+    [SerializeField] private Slider MasterSlider;
     [SerializeField] private Slider GameplaySlider;
     [SerializeField] private Slider MusicSlider;
+    [SerializeField] private Slider VoiceSlider;
+
+    private void Start()
+    {
+        LoadVolume();
+    }
 
     // sets the master volume according to the slider.
-    public void SetMasterVolume()
+    public void SetMasterVolume(float volume)
     {
-        float volume = MainSlider.value;
-
-        audioMixer.SetFloat("MasterMix", Mathf.Log10(volume) * 20);
+        audioMixer.SetFloat("MasterMix", volume);
     }
 
     // sets the music volume according to the slider.
-    public void SetMusicVolume()
+    public void SetMusicVolume(float volume)
     {
-        float volume = MainSlider.value;
-
-        audioMixer.SetFloat("MusicMix", Mathf.Log10(volume) * 20);
+        audioMixer.SetFloat("MusicMix", volume);
     }
 
     // sets the gameplay volume according to the slider.
-    public void SetGameplayVolume() 
+    public void SetGameplayVolume(float volume) 
     {
-        float volume = MainSlider.value;
-
-        audioMixer.SetFloat("GameplayMix", Mathf.Log10(volume) * 20);
+        audioMixer.SetFloat("GameplayMix", volume);
     }
 
+    // sets the voice volume according to slider
+    public void SetVoiceVolume(float volume)
+    {
+        audioMixer.SetFloat("VoiceMix", volume);
+    }
+
+    // saves the volume to the player prefs
+    // avoids the player having to redo audio settings each launch.
+    public void SaveVolume()
+    {
+        // saves master volume
+        audioMixer.GetFloat("MasterMix", out float masterVolume);
+        PlayerPrefs.SetFloat("MasterMix", masterVolume);
+
+        // saves music volume
+        audioMixer.GetFloat("MusicMix", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicMix", musicVolume);
+
+        // saves gameplay volume
+        audioMixer.GetFloat("GameplayMix", out float gameplayVolume);
+        PlayerPrefs.SetFloat("GameplayMix", gameplayVolume);
+
+        // saves voice volume
+        audioMixer.GetFloat("VoiceMix", out float voiceVolume);
+        PlayerPrefs.SetFloat("VoiceMix", voiceVolume);
+    }
+
+    // loads the volume settings saved to player prefs.
+    public void LoadVolume()
+    {
+        MasterSlider.value = PlayerPrefs.GetFloat("MasterMix");
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicMix");
+        GameplaySlider.value = PlayerPrefs.GetFloat("GameplayMix");
+        VoiceSlider.value = PlayerPrefs.GetFloat("VoiceMix");
+    }
 
 }
