286e59e213bf10bd36c498e76424b04c5e10e84b
DM <dommcneill@outlook.com>
2025-05-29 13:51:32 +0100

    player animatison tweaked
    animations settings for animations were adjusted.
    heavy attack range indicator
    player attack redone

--- Assets/Programming/Enemies/Scripts/Damage Ring Indicator/DamageRingIndicator.cs
+++ Assets/Programming/Enemies/Scripts/Damage Ring Indicator/DamageRingIndicator.cs
@@ -2,6 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//by    _                 _ _                     
+//     | |               (_) |                    
+//   __| | ___  _ __ ___  _| |__  _ __ ___  _ __  
+//  / _` |/ _ \| '_ ` _ \| | '_ \| '__/ _ \| '_ \ 
+// | (_| | (_) | | | | | | | |_) | | | (_) | | | |
+//  \__,_|\___/|_| |_| |_|_|_.__/|_|  \___/|_| |_|
+
+
 public class DamageRingIndicator : MonoBehaviour
 {
     public GameObject damageRingGameObject;
