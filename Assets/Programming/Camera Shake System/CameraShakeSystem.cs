96e65d846177704f5b971fc4637aba33b4108f17
DM <dommcneill@outlook.com>
2025-07-14 20:33:39 +0100

    Camera shake, knight serrated slash cone

--- Assets/Programming/Camera Shake System/CameraShakeSystem.cs
+++ Assets/Programming/Camera Shake System/CameraShakeSystem.cs
@@ -2,9 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//by    _                 _ _                     
+//     | |               (_) |                    
+//   __| | ___  _ __ ___  _| |__  _ __ ___  _ __  
+//  / _` |/ _ \| '_ ` _ \| | '_ \| '__/ _ \| '_ \ 
+// | (_| | (_) | | | | | | | |_) | | | (_) | | | |
+//  \__,_|\___/|_| |_| |_|_|_.__/|_|  \___/|_| |_|
+// © 2025 Dominic McNeill dommcneill@outlook.com
+
 public class CameraShakeSystem : MonoBehaviour
 {
     public static CameraShakeSystem instance;
+
+    public float fadeAwayCutOffThreshold = 0.3f;
+
+
+    private Transform cameraShakeTransform;
+    private Vector3 cameraPosition;
+
+    private float shakeTimer = 0f;
+    private float totalShakeTime = 0f;
+    private float verticalShakeFrequency = 0f;
+    private float horizontalShakeFrequency = 0f;
+    private float shakeIntensity = 0f;
 
     void Awake()
     {
@@ -15,18 +35,46 @@ public class CameraShakeSystem : MonoBehaviour
         else
         {
             instance = this;
+
+            cameraShakeTransform = Camera.main.transform.parent;
+            cameraPosition = cameraShakeTransform.localPosition;
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (shakeTimer > 0f)
+        {
+            shakeTimer -= Time.deltaTime;
 
+            float shakeFadeAway = shakeTimer / totalShakeTime;
+            if (shakeFadeAway < fadeAwayCutOffThreshold) shakeFadeAway = 0f; // we set to zero to get rid of micro shakes.
+
+            cameraShakeTransform.localPosition = cameraPosition
+                + new Vector3(Mathf.Sin(shakeTimer * horizontalShakeFrequency) * shakeIntensity * shakeFadeAway, Mathf.Sin(shakeTimer * verticalShakeFrequency) * shakeIntensity * shakeFadeAway, 0);
+
+        }
+        else
+        {
+            cameraShakeTransform.localPosition = cameraPosition;
+        }
+    }
+
+
+    /// <summary>
+    /// Starts the camera shake.
+    /// </summary>
+    /// <param name="duration">How long the shake should last.</param>
+    /// <param name="shakeIntensity">How much the camera should move.</param>
+    /// <param name="verticalShakeFrequency">How much the camera should modulate vertically (up and down).</param>
+    /// <param name="horizontalShakeFrequency">How much the camera should modulate horizontally (lest and right).</param>
+    public void StartShake(float duration, float shakeIntensity = 0.05f, float verticalShakeFrequency = 50f, float horizontalShakeFrequency = 20f)
+    {
+        shakeTimer = duration;
+        totalShakeTime = duration;
+        this.shakeIntensity = shakeIntensity;
+        this.verticalShakeFrequency = verticalShakeFrequency;
+        this.horizontalShakeFrequency = horizontalShakeFrequency;
     }
 }
