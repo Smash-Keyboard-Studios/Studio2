66218b0a1bcdf57c04d3d74e3f14fd0c3af107bc
DM <dommcneill@outlook.com>
2025-07-15 16:53:50 +0100

    Created easing class with a few easing funtions

---
+++ Assets/Programming/Util/Easing/EasingFunctions.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//by    _                 _ _                     
+//     | |               (_) |                    
+//   __| | ___  _ __ ___  _| |__  _ __ ___  _ __  
+//  / _` |/ _ \| '_ ` _ \| | '_ \| '__/ _ \| '_ \ 
+// | (_| | (_) | | | | | | | |_) | | | (_) | | | |
+//  \__,_|\___/|_| |_| |_|_|_.__/|_|  \___/|_| |_|
+
+// Anyone apart of SKS can edit this script.
+
+public class EasingFunctions
+{
+    public static float EaseOutBack(float x)
+    {
+
+        float c1 = 1.70158f;
+        float c3 = c1 + 1;
+
+        return 1 + c3 * Mathf.Pow(x - 1, 3) + c1 * Mathf.Pow(x - 1, 2);
+    }
+
+    public static float EaseInOutCubic(float x)
+    {
+        return x < 0.5 ? 4 * x * x * x : 1 - Mathf.Pow(-2 * x + 2, 3) / 2;
+    }
+
+    public static float EaseOutQuint(float x)
+    {
+        return 1 - Mathf.Pow(1 - x, 5);
+    }
+}
